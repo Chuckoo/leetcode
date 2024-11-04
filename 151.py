47f8be69be232e507ad9c340c2f015c049eb6214
Chuckoo-l2l <vivek279.venky@gmail.com>
2024-11-03 22:09:41 -0800

    151

--- 151.py
+++ 151.py
@@ -2,4 +2,4 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         return ' '.join(s.strip().split()[-1::-1])
 
-print(Solution().reverseWords("the  sky   is  blue  "))
+print(Solution().reverseWords("  the  sky   is  blue  "))
