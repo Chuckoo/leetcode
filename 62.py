97fe4f16fb411a04f103edb6db5b9520b4da411a
Vivek <vivek279.venky@gmail.com>
2024-11-28 23:00:09 -0800

    62

---
+++ 62.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
+
+
+m = 3
+n = 7
+print(Solution().uniquePaths(m,n))
